c043a9d32377f121fbee854320a83891b2c26626
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-20 17:50:12 +0700

    buat function TampilSiswa

--- app/src/main/java/com/example/prak8/view/TampilSiswa.kt
+++ app/src/main/java/com/example/prak8/view/TampilSiswa.kt
@@ -1,2 +1,21 @@
 package com.example.prak8.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.prak8.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusUiSiswa: Siswa,
+    onBackButtonClick: () -> Unit
+) {
+    val item = listOf(
+        Pair(stringResource("Nama Lengkap"), statusUiSiswa.nama),
+        Pair(stringResource("Jenis Kelamin"), statusUiSiswa.gender),
+        Pair(stringResource("Alamat"), statusUiSiswa.alamat)
+    )
+
+
+}
