c7e8b7ba44e5e621bb7b95fcf94dfb9dc3230787
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-20 18:16:19 +0700

    buat function SiswaApp

--- app/src/main/java/com/example/prak8/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/prak8/view/uicontroller/Navigasi.kt
@@ -1,2 +1,19 @@
 package com.example.prak8.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.prak8.viewmodel.SiswaViewModel
+
+@OptIn(ExperimentalMaterial3Api)
+@Composable
+fun SiswaApp(
+    modifier: Modifier,
+    viewModel: SiswaViewModel = ViewModel(),
+    navController: NavHostController = rememberNavController()
+){
+
+}
