bf59a0acdf252e83c4501d9d17a9d474740d3d62
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-20 17:01:25 +0700

    mengupdate tampilan UI pada FormSiswa.kt

--- app/src/main/java/com/example/prak8/view/FormSiswa.kt
+++ app/src/main/java/com/example/prak8/view/FormSiswa.kt
@@ -1,4 +1,7 @@
 package com.example.prak8.view
+
+import com.example.prak8.R
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,6 +13,18 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier as Modfier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.foundation.selection.selectable
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,9 +39,43 @@ fun FormSiswa(
     var txtGender by rememberSaveable { mutableStateOf("") }
     var listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
 
-    Scaffold (modifier : Modfier,
-        { TopAppBar(title = { Text(stringResource(id ="Form Pendaftaran"), color = ) }) })
+    Scaffold (modifier = Modfier,
+        { TopAppBar(title = { Text(stringResource(id = R.string.app_name), color = Color.White)},
+        )
+        }) { isiRuang ->
 
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = txtNama,
+                singleline = true,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.width(250.dp).padding(top=20.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = {
+                    txtNama = it
+                }
+            )
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .width(250.dp),
+                thickness = dimensionResource(1dp),
+                color = Color.Blue
+            )
+            Row {
+                pilihanJK.forEach { item ->
+                    Row(modifier = Modifier.selectable(
+                        selected = txtGender == item,
+                        onClick = {
+                            txtGender = item
+                        }
+                    ),
+                }
+            }
+        }
     }
-
 }
