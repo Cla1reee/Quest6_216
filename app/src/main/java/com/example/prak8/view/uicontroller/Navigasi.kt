c82c65bbe25d15cc0f1494109bc6a0524dba76d9
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-20 18:24:22 +0700

    memperbarui Navigasi.kt

--- app/src/main/java/com/example/prak8/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/prak8/view/uicontroller/Navigasi.kt
@@ -1,12 +1,25 @@
 package com.example.prak8.view.uicontroller
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.prak8.view.FormSiswa
 import com.example.prak8.viewmodel.SiswaViewModel
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.layout.padding
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
 
 @OptIn(ExperimentalMaterial3Api)
 @Composable
@@ -15,5 +28,23 @@ fun SiswaApp(
     viewModel: SiswaViewModel = ViewModel(),
     navController: NavHostController = rememberNavController()
 ){
+    Scaffold { isiRuang ->
+        val uiState = ViewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
 
+            modifier = Modifier.padding(isiRuang)){
+                composable(route = Navigasi.Formulir.name){
+                    val konteks = LocalContext.current
+                    FormSiswa(
+                        pilihanJK = JenisK.map {id -> konteks.resources.getString(id) },
+                        onSubmitButtonClicked = {
+                            viewModel.setSiswa(it)
+                        }
+                    )
+                }
+            }
+        )
+    }
 }
