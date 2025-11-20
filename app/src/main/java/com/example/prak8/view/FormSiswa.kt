ccb5d4db16c9a63a39d4ff677c348d3421af6d20
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-20 19:26:22 +0700

    memperbaiki syntax dan layout pada FormSiswa.kt

--- app/src/main/java/com/example/prak8/view/FormSiswa.kt
+++ app/src/main/java/com/example/prak8/view/FormSiswa.kt
@@ -1,25 +1,36 @@
 package com.example.prak8.view
 
-import com.example.prak8.R
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Button
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.colorResource
-
+import com.example.prak8.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,13 +42,16 @@ fun FormSiswa(
     var txtNama by rememberSaveable { mutableStateOf("") }
     var txtAlamat by rememberSaveable { mutableStateOf("") }
     var txtGender by rememberSaveable { mutableStateOf("") }
-    var listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
 
-    Scaffold (modifier = Modfier,
-        { TopAppBar(title = { Text(stringResource(id = R.string.app_name), color = Color.White)},
-            colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.purple_500))
-        )
-        }) { isiRuang ->
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(id = R.string.app_name), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.purple_500))
+            )
+        }
+    ) { isiRuang ->
 
         Column(
             modifier = Modifier.padding(isiRuang),
@@ -48,7 +62,7 @@ fun FormSiswa(
                 value = txtNama,
                 singleLine = true,
                 shape = MaterialTheme.shapes.medium,
-                modifier = Modifier.width(250.dp).padding(top=20.dp),
+                modifier = Modifier.width(250.dp).padding(top = 20.dp),
                 label = { Text(text = "Nama Lengkap") },
                 onValueChange = {
                     txtNama = it
@@ -58,17 +72,18 @@ fun FormSiswa(
                 modifier = Modifier
                     .padding(12.dp)
                     .width(250.dp),
-                thickness = dimensionResource(1dp),
+                thickness = 1.dp,
                 color = Color.Blue
             )
             Row {
                 pilihanJK.forEach { item ->
-                    Row(modifier = Modifier.selectable(
-                        selected = txtGender == item,
-                        onClick = {
-                            txtGender = item
-                        }
-                    ),
+                    Row(
+                        modifier = Modifier.selectable(
+                            selected = txtGender == item,
+                            onClick = {
+                                txtGender = item
+                            }
+                        ),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         RadioButton(
@@ -85,7 +100,7 @@ fun FormSiswa(
                 modifier = Modifier
                     .padding(5.dp)
                     .width(250.dp),
-                thickness = dimensionResource(1dp),
+                thickness = 1.dp,
                 color = Color.Blue
             )
             OutlinedTextField(
@@ -102,9 +117,9 @@ fun FormSiswa(
             Button(
                 modifier = Modifier.fillMaxWidth(1f),
                 enabled = txtAlamat.isNotEmpty(),
-                onClick = {onSubmitButtonClicked(listData)}
+                onClick = { onSubmitButtonClicked(mutableListOf(txtNama, txtGender, txtAlamat)) }
             ) {
-                Text(stringResource("Submit"))
+                Text(stringResource(R.string.submit))
             }
         }
     }
