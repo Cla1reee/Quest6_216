aca81d9998f67cbe643cd109604cf21183370a91
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2025-11-20 19:27:10 +0700

    memperbarui Navigasi.kt untuk menangani navigasi ke TampilSiswa

--- app/src/main/java/com/example/prak8/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/prak8/view/uicontroller/Navigasi.kt
@@ -1,19 +1,21 @@
 package com.example.prak8.view.uicontroller
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.ViewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.example.prak8.view.FormSiswa
-import com.example.prak8.viewmodel.SiswaViewModel
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.foundation.layout.padding
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.prak8.model.DataJK.JenisK
+import com.example.prak8.view.FormSiswa
+import com.example.prak8.viewmodel.SiswaViewModel
+import com.example.prak8.view.TampilSiswa
 
 
 enum class Navigasi {
@@ -21,30 +23,43 @@ enum class Navigasi {
     Detail
 }
 
-@OptIn(ExperimentalMaterial3Api)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaApp(
     modifier: Modifier,
-    viewModel: SiswaViewModel = ViewModel(),
+    viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ){
     Scaffold { isiRuang ->
-        val uiState = ViewModel.statusUI.collectAsState()
+        val uiState = viewModel.statusUI.collectAsState()
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulir.name,
 
-            modifier = Modifier.padding(isiRuang)){
-                composable(route = Navigasi.Formulir.name){
-                    val konteks = LocalContext.current
-                    FormSiswa(
-                        pilihanJK = JenisK.map {id -> konteks.resources.getString(id) },
-                        onSubmitButtonClicked = {
-                            viewModel.setSiswa(it)
-                        }
-                    )
-                }
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulir.name) {
+                val konteks = LocalContext.current
+                FormSiswa(
+                    pilihanJK = JenisK.map { id -> konteks.resources.getString(id) },
+                    onSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
             }
-        )
+            composable(route = Navigasi.Detail.name) {
+                TampilSiswa(
+                    statusUiSiswa = uiState.value,
+                    onBackButtonClick = { cancelAndBackToFormulir(navController) }
+                )
+            }
+        }
     }
+}
+
+private fun  cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(Navigasi.Formulir.name, inclusive = false)
 }
